dd564d562f809057c01f62fe51414014d0cc0c49
Marcin <marcin@Workstation.fios-router.home>
2018-09-22 10:46:52 -0400

    renamed pkg

--- app/main.go
+++ app/main.go
@@ -7,7 +7,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 
-	dbaccess "../pkg/db"
+	utils "../pkg/db-utils"
 )
 
 func main() {
@@ -20,7 +20,7 @@ func main() {
 	rows, err := db.Queryx(`select * from test_table t1 where t1.version = 
 		(select max(t2.version) from test_table t2 where t1.id = t2.id)`)
 
-	ms, err := dbaccess.ScanIntoMaps(rows)
+	ms, err := utils.ScanIntoMaps(rows)
 
 	log.Println("Displaying maps as json:")
 
